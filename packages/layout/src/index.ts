dda2cdbdda901a59b8575d459f14df006bdddbe6
Osama <agilegroupai@gmail.com>
2026-09-17 07:21:01 +0300

    Temporary transfer objects 1/84

--- packages/layout/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./units.js";
-export * from "./linebreak.js";
-export * from "./vertical.js";
--- reference/dart-engine/lib/wordToHTML/GlyphEncodedDigitFonts.dart
+++
@@ -1,8 +0,0 @@
-const Set<String> glyphEncodedDigitFonts = {
-  'QCF_BSML',
-};
-
-bool shouldPreserveWesternDigitsForFontFamily(String? fontFamily) {
-  if (fontFamily == null) return false;
-  return glyphEncodedDigitFonts.contains(fontFamily.trim().toUpperCase());
-}
