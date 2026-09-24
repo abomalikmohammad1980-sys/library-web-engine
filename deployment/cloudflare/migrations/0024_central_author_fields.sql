3580d747142c2a924bb5329a7e123799ed2bad16
abomalikmohammad1980-sys <abomalikmohammad1980@gmail.com>
2026-09-24 22:21:11 +0300

    Make trigger CASE guards D1-compatible

--- deployment/cloudflare/migrations/0024_central_author_fields.sql
+++ deployment/cloudflare/migrations/0024_central_author_fields.sql
@@ -4,14 +4,15 @@ DROP TRIGGER oversight_central_authors_insert;
 DROP TRIGGER oversight_central_authors_update;
 CREATE TRIGGER oversight_central_authors_insert AFTER INSERT ON central_authors WHEN 1
 BEGIN
- SELECT CASE WHEN NOT (EXISTS(SELECT 1 FROM accounts a WHERE a.subject=NEW.updated_by AND (a.role='super-admin' OR (a.role='user' AND EXISTS(SELECT 1 FROM account_capabilities c WHERE c.subject=a.subject AND editorial=1)))) AND NOT EXISTS(SELECT 1 FROM account_blocks WHERE subject=NEW.updated_by AND blocked=1)) THEN RAISE(ABORT,'oversight_actor_forbidden') END;
+ SELECT (CASE WHEN NOT (EXISTS(SELECT 1 FROM accounts a WHERE a.subject=NEW.updated_by AND (a.role='super-admin' OR (a.role='user' AND EXISTS(SELECT 1 FROM account_capabilities c WHERE c.subject=a.subject AND editorial=1)))) AND NOT EXISTS(SELECT 1 FROM account_blocks WHERE subject=NEW.updated_by AND blocked=1)) THEN RAISE(ABORT,'oversight_actor_forbidden') END);
  INSERT INTO oversight_events(entity_type,entity_id,actor_subject,action,before_json,after_json,before_revision,revision)
  VALUES('central-author',NEW.author_id,NEW.updated_by,'create',NULL,json_object('display_name',NEW.display_name,'biography',NEW.biography,'source',NEW.source,'death_year_hijri',NEW.death_year_hijri,'contemporary',NEW.contemporary,'hidden_at',NEW.hidden_at,'fields_json',NEW.fields_json),0,NEW.revision);
 END;
 CREATE TRIGGER oversight_central_authors_update AFTER UPDATE ON central_authors WHEN NEW.revision<>OLD.revision
 BEGIN
- SELECT CASE WHEN NOT (EXISTS(SELECT 1 FROM accounts a WHERE a.subject=NEW.updated_by AND (a.role='super-admin' OR (a.role='user' AND EXISTS(SELECT 1 FROM account_capabilities c WHERE c.subject=a.subject AND editorial=1)))) AND NOT EXISTS(SELECT 1 FROM account_blocks WHERE subject=NEW.updated_by AND blocked=1)) THEN RAISE(ABORT,'oversight_actor_forbidden') END;
+ SELECT (CASE WHEN NOT (EXISTS(SELECT 1 FROM accounts a WHERE a.subject=NEW.updated_by AND (a.role='super-admin' OR (a.role='user' AND EXISTS(SELECT 1 FROM account_capabilities c WHERE c.subject=a.subject AND editorial=1)))) AND NOT EXISTS(SELECT 1 FROM account_blocks WHERE subject=NEW.updated_by AND blocked=1)) THEN RAISE(ABORT,'oversight_actor_forbidden') END);
  INSERT INTO oversight_events(entity_type,entity_id,actor_subject,action,before_json,after_json,before_revision,revision)
  VALUES('central-author',NEW.author_id,NEW.updated_by,'update',json_object('display_name',OLD.display_name,'biography',OLD.biography,'source',OLD.source,'death_year_hijri',OLD.death_year_hijri,'contemporary',OLD.contemporary,'hidden_at',OLD.hidden_at,'fields_json',OLD.fields_json),json_object('display_name',NEW.display_name,'biography',NEW.biography,'source',NEW.source,'death_year_hijri',NEW.death_year_hijri,'contemporary',NEW.contemporary,'hidden_at',NEW.hidden_at,'fields_json',NEW.fields_json),OLD.revision,NEW.revision);
 END;
+
 
